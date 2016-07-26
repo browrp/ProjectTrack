d0aceba3902c49a6d27868b82ef800fa5c535a9e
Robert Brown <robertbrown@macbook-pro-10.unih.uc.edu>
2016-07-26 14:15:26 -0400

    Reverting to inheriting from ActiveModelSerializers::Model and removing Project initializer.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,34 +1,23 @@
-# #This version of the class will throw an error
-# class Project < ActiveModelSerializers::Model
-#
-#   #commenting this out will cause the error to stop but
-#   #json is empty
-#   def initialize
-#     project_sponsors = Array.new
-#   end
-#
-#   attr_accessor :uuid_key, :project_name
-#   attr_accessor :project_sponsors
-#
-# end
-
-
-
-#Using this version of the class results in valid JSON
-class Project
-
-  alias :read_attribute_for_serialization :send
+class Project < ActiveModelSerializers::Model
 
   attr_accessor :uuid_key, :project_name
   attr_accessor :project_sponsors
 
-  def self.model_name
-    @_model_name ||= ActiveModel::Name.new(self)
-  end
-
-
-  def initialize
-     project_sponsors = Array.new
-  end
-
 end
+
+
+
+# #Using this version of the class results in valid JSON
+# class Project
+#
+#   alias :read_attribute_for_serialization :send
+#
+#   attr_accessor :uuid_key, :project_name
+#   attr_accessor :project_sponsors
+#
+#   def self.model_name
+#     @_model_name ||= ActiveModel::Name.new(self)
+#   end
+#
+#
+# end
--- app/models/project_sponsor.rb
+++ app/models/project_sponsor.rb
@@ -1,18 +1,18 @@
-# class ProjectSponsor < ActiveModelSerializers::Model
-#   attr_accessor :uuid_key, :first_name, :last_name
-# end
-
-
-
-class ProjectSponsor
-
-  alias :read_attribute_for_serialization :send
-
-
+class ProjectSponsor < ActiveModelSerializers::Model
   attr_accessor :uuid_key, :first_name, :last_name
-
-  def self.model_name
-    @_model_name ||= ActiveModel::Name.new(self)
-  end
-
 end
+
+
+
+# class ProjectSponsor
+#
+#   alias :read_attribute_for_serialization :send
+#
+#
+#   attr_accessor :uuid_key, :first_name, :last_name
+#
+#   def self.model_name
+#     @_model_name ||= ActiveModel::Name.new(self)
+#   end
+#
+# end
